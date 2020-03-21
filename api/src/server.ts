7647af792453b422252f905080eb5fc02b8db606
João Victor <jvsouzax@gmail.com>
2020-03-21 14:28:13 -0300

    add: express

--- api/src/server.ts
+++ api/src/server.ts
@@ -1,2 +1,10 @@
-const sum = (a: number, b: number) => a + b;
-setInterval(() => console.log(sum(2, 3)), 1000);
+import http from "http";
+import express from "express";
+
+const router = express();
+const { PORT = 3000 } = process.env;
+const server = http.createServer(router);
+
+server.listen(PORT, () =>
+  console.log(`Server is running http://localhost:${PORT}...`)
+);
