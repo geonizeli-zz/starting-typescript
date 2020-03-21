d6566941e4f0bba2aa33a81858735c8a97331ed8
João Victor <jvsouzax@gmail.com>
2020-03-21 10:24:00 -0300

    add: tuple example

---
+++ README.md
@@ -0,0 +1,8 @@
+# helloWorld.ts
+- Basic variable typed example
+
+# fibonacci.ts
+- Array declaration
+
+# tuple.ts
+- Tuple example
---
+++ tuple.ts
@@ -0,0 +1,14 @@
+let x: [string, number] = ["String", 19];
+
+x.forEach(element => {
+  console.log(element)
+});
+
+/* OUTPUT
+```
+  var x = ["String", 19];
+  x.forEach(function (element) {
+      console.log(element);
+  });
+```
+*/
